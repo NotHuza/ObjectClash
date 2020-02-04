7d55d09189fc62d56efa61c1276487971effb2e7
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-02-04 12:24:55 +0100

    Create ServiceNode.cs

---
+++ ServiceNode.cs
@@ -0,0 +1,14 @@
+
+
+namespace CR.Servers.CoC.Packets.Enums
+{
+    internal enum ServiceNode
+    {
+        Account = 1,
+        Friend = 3,
+        GlobalChat = 6,
+        Avatar = 9,
+        Home = 10,
+        Alliance = 11,
+    }
+}
