490f445e5012a7913bf1e227e2498738e928b0b4
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-07 00:45:06 +0300

    Update AssemblyInfo.cs

--- src/CR.Servers/CR.Servers.CoC/Properties/AssemblyInfo.cs
+++ src/CR.Servers/CR.Servers.CoC/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CR.Servers.CoC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET Clash of Clans v9.256.20 Server Emulator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ultapower")]
 [assembly: AssemblyProduct("CR.Servers.CoC")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.5.0")]
+[assembly: AssemblyFileVersion("1.4.5.0")]
