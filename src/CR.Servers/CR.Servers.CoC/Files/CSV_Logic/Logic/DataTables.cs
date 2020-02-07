0bbf68c5936dd23a5e4275e3272f4308bc189df9
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-07 22:31:32 +0300

    Update DataTables.cs

--- src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/DataTables.cs
+++ src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/DataTables.cs
@@ -3,7 +3,7 @@ using CR.Servers.CoC.Files.CSV_Helpers;
 using CR.Servers.CoC.Logic.Enums;
 using CR.Servers.Files.CSV_Reader;
 
-namespace CR.Servers.CoC.Files.CSV_Helpers
+namespace CR.Servers.CoC.Files.CSV_Logic.Logic
 {
     internal class DataTables
     {
