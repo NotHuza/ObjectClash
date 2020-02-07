9cb75fe345b2172e2eeaf4da1b15a87da6150bee
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-02-07 17:40:22 +0100

    clean

--- src/CR.Servers/CR.Servers.CoC/Program.cs
+++ src/CR.Servers/CR.Servers.CoC/Program.cs
@@ -22,7 +22,7 @@ namespace CR.Servers.CoC
         private static void Main()
         {
             Configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Config.json").Build();
-            Console.Title =  $"Huza Clash - {Assembly.GetExecutingAssembly().GetName().Name} - {DateTime.Now.Year} ©";
+            Console.Title = $"Huza Clash - {Assembly.GetExecutingAssembly().GetName().Name} - {DateTime.Now.Year} ©";
 
             Console.SetOut(new Prefixed());
             Console.SetWindowSize(Width, Height);
@@ -36,7 +36,7 @@ namespace CR.Servers.CoC
                    \/             \/     \/          \/          \/     \/     \/                                                                                                            Clash Edition
             ", Color.OrangeRed, Color.LimeGreen, 14);
 
-            Console.ForegroundColor = ComsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(@"Ultarpowe Developer Team: Antz, Huza Modz, MeteHun, Alin");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(Environment.NewLine);
@@ -45,8 +45,8 @@ namespace CR.Servers.CoC
             Console.WriteLine(@"Huza Clash is NOT affiliated to 'Supercell Oy'.");
             Console.WriteLine(@"Huza Clash does NOT own 'Clash of Clans', 'Boom Beach', 'Clash Royale'.");
             Console.WriteLine();
-            Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Name + " is now starting..." +  Environment.NewLine);
-            
+            Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Name + " is now starting..." + Environment.NewLine);
+
             Resources.Initialize();
             Thread.Sleep(-1);
         }
