9bf8736eff11b34c91be08481616e33bb47aebf8
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-09 00:04:19 +0300

    Update ResourceData.cs

--- src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/ResourceData.cs
+++ src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/ResourceData.cs
@@ -1,32 +1,66 @@
+using CR.Servers.CoC.Files.CSV_Helpers;
+using CR.Servers.Files.CSV_Reader;
+
 ﻿namespace CR.Servers.CoC.Files.CSV_Logic.Logic
 {
-    using CR.Servers.CoC.Files.CSV_Helpers;
-    using CR.Servers.Files.CSV_Reader;
-
     internal class ResourceData : Data
     {
-        public ResourceData(Row Row, DataTable DataTable) : base(Row, DataTable)
+        internal ResourceData warRefResourceD;
+
+        internal EffectData collectEffectD;
+        internal EffectData stealEffectD;
+        internal EffectData stealEffectMidD;
+        internal EffectData stealEffectBigD;
+
+        public ResourceData(Row rw, DataTable dt) : base(rw, dt)
         {
         }
 
         public override string Name { get; set; }
+
         public string TID { get; set; }
+
         public string SWF { get; set; }
+
         public string CollectEffect { get; set; }
+
         public string ResourceIconExportName { get; set; }
+
         public string StealEffect { get; set; }
+
         public int StealLimitMid { get; set; }
+
         public string StealEffectMid { get; set; }
+
         public int StealLimitBig { get; set; }
+
         public string StealEffectBig { get; set; }
+
         public bool PremiumCurrency { get; set; }
+
         public string HudInstanceName { get; set; }
+
         public string CapFullTID { get; set; }
+
         public int TextRed { get; set; }
+
         public int TextGreen { get; set; }
+
         public int TextBlue { get; set; }
+
         public string WarRefResource { get; set; }
+
         public string BundleIconExportName { get; set; }
+
         public int VillageType { get; set; }
+
+        internal override void Process()
+        {
+            this.warRefResourceD = DataTables.GetResourceByName(this.WarRefResource);
+            this.collectEffectD = DataTables.GetEffectByName(this.﻿CollectEffect);
+            this.stealEffectD = DataTables.GetEffectByName(this.StealEffect);
+            this.stealEffectMidD = DataTables.GetEffectByName(this.StealEffectMid);
+            this.stealEffectBigD = DataTables.GetEffectByName(this.StealEffectBig);
+        }
     }
 }
