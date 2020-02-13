3c87f906376615c1f6eb1a6a0ea9f39e0e795965
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-13 15:31:15 +0300

    Update AllianceBadgeLayerData.cs

--- src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/AllianceBadgeLayerData.cs
+++ src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/AllianceBadgeLayerData.cs
@@ -1,17 +1,22 @@
-﻿namespace CR.Servers.CoC.Files.CSV_Logic.Logic
-{
-    using CR.Servers.CoC.Files.CSV_Helpers;
-    using CR.Servers.Files.CSV_Reader;
+﻿using CR.Servers.CoC.Files.CSV_Helpers;
+using CR.Servers.Files.CSV_Reader;
 
+namespace CR.Servers.CoC.Files.CSV_Logic.Logic
+{
     internal class AllianceBadgeLayerData : Data
     {
-        public AllianceBadgeLayerData(Row Row, DataTable DataTable) : base(Row, DataTable)
+        public AllianceBadgeLayerData(Row rw, DataTable dt) : base(rw, dt)
         {
         }
 
         public override string Name { get; set; }
+
         public string Type { get; set; }
+
         public string SWF { get; set; }
+
         public string ExportName { get; set; }
+
+        public int RequiredClanLevel { get; set; }
     }
 }
