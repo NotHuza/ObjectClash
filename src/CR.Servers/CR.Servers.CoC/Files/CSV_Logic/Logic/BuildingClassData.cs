634a61889410f1d484640b300cf06aa11ddc016e
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-02-13 15:32:40 +0300

    Update BuildingClassData.cs

--- src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/BuildingClassData.cs
+++ src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/BuildingClassData.cs
@@ -1,18 +1,24 @@
-﻿namespace CR.Servers.CoC.Files.CSV_Logic.Logic
-{
-    using CR.Servers.CoC.Files.CSV_Helpers;
-    using CR.Servers.Files.CSV_Reader;
+﻿using CR.Servers.CoC.Files.CSV_Helpers;
+using CR.Servers.Files.CSV_Reader;
 
+namespace CR.Servers.CoC.Files.CSV_Logic.Logic
+{
     internal class BuildingClassData : Data
     {
-        public BuildingClassData(Row Row, DataTable DataTable) : base(Row, DataTable)
+        public BuildingClassData(Row rw, DataTable dt) : base(rw, dt)
         {
         }
 
         public override string Name { get; set; }
+
         public string TID { get; set; }
+
         public bool CanBuy { get; set; }
+
         public bool ShopCategoryResource { get; set; }
+
         public bool ShopCategoryArmy { get; set; }
+
+        public bool ShopCategoryDefense { get; set; }
     }
 }
