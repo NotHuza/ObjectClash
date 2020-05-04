ce1069edab724363f35e0492ba015abf16e0c942
Antz <15341485+antzsmt@users.noreply.github.com>
2020-05-04 21:27:33 +0800

    update

---
+++ src/CR.Servers/CR.Servers.CoC/Packets/Enums/ServiceNode.cs
@@ -0,0 +1,18 @@
+﻿namespace CR.Servers.CoC.Packets.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal enum ServiceNode
+    {
+        Account = 1,
+        Friend = 3,
+        GlobalChat = 6,
+        Avatar = 9,
+        Home = 10,
+        Alliance = 11,
+    }
+}
