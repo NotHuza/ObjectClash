2cbe840ecf39f52077e2169f36810e66ef9984dd
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-05-04 15:52:13 +0200

    Add files via upload

---
+++ src/CR.Servers/CR.Servers.CoC/Extensions/specialExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR.Servers.CoC.Extensions
+{
+    public static class SpecialExtensions
+    {
+        public static string ToReadableString(this TimeSpan span)
+        {
+            var formatted =
+                $"{(span.Duration().Days > 0 ? $"{span.Days:0}d, " : string.Empty)}{(span.Duration().Hours > 0 ? $"{span.Hours:0}h, " : string.Empty)}{(span.Duration().Minutes > 0 ? $"{span.Minutes:0}m, " : string.Empty)}{(span.Duration().Seconds > 0 ? $"{span.Seconds:0}s" : string.Empty)}";
+
+            if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+            if (string.IsNullOrEmpty(formatted)) formatted = "0s";
+
+            return formatted;
+        }
+    }
+}
