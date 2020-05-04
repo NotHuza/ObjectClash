72afaccc30c331740ebdfff788d70f4e7f7fa409
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-05-04 15:59:10 +0200

    small cacage

--- src/CR.Servers/CR.Servers.CoC/Packets/Debug.cs
+++ src/CR.Servers/CR.Servers.CoC/Packets/Debug.cs
@@ -5,6 +5,7 @@
     using CR.Servers.CoC.Logic;
     using CR.Servers.CoC.Logic.Clan;
     using CR.Servers.CoC.Packets.Messages.Server.Home;
+    using CR.Servers.Extensions;
     using CR.Servers.Logic.Enums;
 
     internal class Debug : IDisposable
@@ -15,7 +16,31 @@
         internal int ParameterOffset;
 
         internal string[] Parameters;
+        private string PlayerTag;
 
+        internal int PlayerLowID
+        {
+            get
+            {
+                return (int)Tags.ToId(this.PlayerTag);
+            }
+        }
+
+        internal int PlayerHighID
+        {
+            get
+            {
+                return (int)(Tags.ToId(this.PlayerTag) >> 32);
+            }
+        }
+
+      
+
+
+        public Debug(string[] parameters)
+        {
+            Parameters = parameters;
+        }
 
         internal Debug(Device Device, params string[] Parameters)
         {
@@ -39,6 +64,11 @@
             }
         }
 
+        internal virtual void Decode()
+        {
+            this.PlayerTag = this.NextParameter;
+        }
+
         public virtual void Dispose()
         {
             this.Parameters = null;
@@ -54,7 +84,7 @@
             new GlobalChatLineMessage(this.Device, this.Device.GameMode.Level.Player)
             {
                 Message = message,
-                Name = "[System] Command Manager",
+                Name = "You",
                 ExpLevel = 300,
                 League = 22,
                 Bot = true
