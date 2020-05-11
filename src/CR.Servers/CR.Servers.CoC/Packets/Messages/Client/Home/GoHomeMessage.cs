7e3b2fe61b422e98441f6094b16d76032244d44d
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-05-11 15:05:43 +0200

    add win trofeis

--- src/CR.Servers/CR.Servers.CoC/Packets/Messages/Client/Home/GoHomeMessage.cs
+++ src/CR.Servers/CR.Servers.CoC/Packets/Messages/Client/Home/GoHomeMessage.cs
@@ -5,11 +5,13 @@
     using CR.Servers.CoC.Packets.Messages.Server.Home;
     using CR.Servers.Extensions.Binary;
     using CR.Servers.Logic.Enums;
+    using System;
     using System.Threading.Tasks;
 
     internal class GoHomeMessage : Message
     {
         internal int Mode;
+        static Player Player;
 
         public GoHomeMessage(Device device, Reader reader) : base(device, reader)
         {
@@ -37,20 +39,28 @@
             }
             else
             {
+                Player = this.Device.GameMode.Level.Player;
                 if (this.Device.State == State.IN_PC_BATTLE)
                 {
                     if (this.Device.Account.Battle != null)
                     {
+                        //Player.Score = this.Device.Account.Battle.LostScore;
+                        Console.WriteLine(@"lost");
                         await this.Device.Account.Battle.EndBattleAsync();
-                        this.Device.Account.Battle = null;
+                        Player.Score = this.Device.Account.Battle.WinScore;
+                        this.Device.Account.Battle = null;//npc
+          
+                        
                     }
                 }
 
                 if (this.Device.Account.DuelBattle != null)
                 {
+                   
                     await this.Device.Account.DuelBattle.GetBattle(this.Device.GameMode.Level)?.EndBattleAsync();
                     this.Device.Account.DuelBattle.SendDuelBattleInfoMessage();
-                    this.Device.Account.DuelBattle = null;
+                    Player.Score = this.Device.Account.Battle.WinDuelScore;
+                    this.Device.Account.DuelBattle = null;//npc
                 }
 
                 this.Device.State = State.LOGGED;
